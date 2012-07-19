cbdfc13be1486adc872e936e4f6b9074048c0f70
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-07-19 13:51:58 -0700

    Adding views

--- app/controllers/parking_spots_controller.rb
+++ app/controllers/parking_spots_controller.rb
@@ -10,22 +10,22 @@ class ParkingSpotsController < ApplicationController
     @resource = Location.new(params[:price_plan])
     @user = User.find_by_email(params[:user][:email])
     
-    @resource.user_id = @user_id
+    @resource.user_id = @user.id
     
     respond_to do |format|
       if @resource.save
         @location.resource_id = @resource.id
         @price_plan.resource_id = @resource.id
         if @location.save and @price_plan.save
-          format.html { redirect_to @user, notice: 'User was successfully created.' }
-          format.json { render json: @user, status: :created, location: @user }
+          format.html { redirect_to @resource, notice: 'Parking Spot was successfully created.' }
+          format.json { render json: @resource, status: :created, location: @resource }
         else
           format.html { render action: "new" }
-          format.json { render json: @user.errors, status: :unprocessable_entity }
+          format.json { render json: @resource.errors, status: :unprocessable_entity }
         end
       else
         format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.json { render json: @resource.errors, status: :unprocessable_entity }
       end
     end
   end
