cc4a45aecf39c37ec5a57dc99350c63ad47dd5d3
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-07-19 12:39:59 -0700

    Adding views

--- db/migrate/20120719193353_change_acceptances.rb
+++ db/migrate/20120719193353_change_acceptances.rb
@@ -1,7 +1,7 @@
 class ChangeAcceptances < ActiveRecord::Migration
   def change
     rename_column :acceptances, :buyer_id, :user_id
-    rename_column :resources, :vender_id, :user_id
+    rename_column :resources, :vendor_id, :user_id
   end
 
 end
