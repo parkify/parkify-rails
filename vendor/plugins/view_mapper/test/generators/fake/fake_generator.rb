e1d6fe35923547d53e0b18437c7be956f0eb4cc0
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-07-19 11:22:20 -0700

    Adding views

--- vendor/plugins/view_mapper/test/generators/fake/fake_generator.rb
+++
@@ -1,3 +0,0 @@
-class FakeGenerator < ScaffoldGenerator
-  include ViewMapper
-end
--- vendor/plugins/view_mapper/test/views/fake/fake_view.rb
+++
@@ -1,7 +0,0 @@
-module ViewMapper
-  module FakeView
-    def self.source_root
-      File.expand_path(File.dirname(__FILE__) + '/templates')
-    end
-  end
-end
