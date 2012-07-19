83a0c78fbdbe0430d236b5f3c2a109cec184a5e9
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-07-19 05:28:37 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParkifyRails::Application.config.secret_token = 'ce2f10a94140615bd62427c21d47c17026c62dc83c9715cda7f4f7f5da21f257a0c4d903fcc1b5a33e5a0a927aaabe277a7fe002e26b816f9c63d63dcf14e1b6'
