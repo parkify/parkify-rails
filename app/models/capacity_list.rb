7eeca9ad1289986859c5848208fb8e55bd119e92
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-08-05 03:32:23 -0700

    Adding ability to make spot from account

--- app/models/capacity_list.rb
+++ app/models/capacity_list.rb
@@ -9,28 +9,39 @@ class CapacityList < ActiveRecord::Base
   #Note: ti contains quantity as capacity
   def add_if_can!(ti)
     intervals = capacity_intervals.select{|i| i.overlaps_with_interval(ti)}.order('start_time')
-    toAdd = List()
+    toAdd = []
     if(intervals.size == 0) #no matches
       return false
     elsif(intervals.size == 1) #both in one interval
       if(intervals[0].start_time != ti.start_time)
         toAdd << capacity_intervals.new({:capacity=>intervals[0].capacity, :start_time=>intervals[0].start_time, :end_time=>ti.start_time})
       end
-        toAdd << capacity_intervals.new({:capacity=>intervals[0].capacity-ti.capacity, :start_time=>intervals[0].start_time, :end_time=>ti.start_time})
+       
+      toAdd << capacity_intervals.new({:capacity=>intervals[0].capacity-ti.capacity, :start_time=>ti.start_time, :end_time=>ti.end_time})
+      
       if(intervals[0].end_time != ti.end_time)
         toAdd << capacity_intervals.new({:capacity=>intervals[0].capacity, :start_time=>ti.end_time, :end_time=>intervals[0].end_time})
       end
       #split  interval[0] into 3
-    elsif(intervals.size == 2) #both in adjacent intervals
-    #  if(intervals[0].start_time != ti.start_time)
-   #     toAdd << capacity_intervals.new({:capacity=>intervals[0].capacity, :start_time=>intervals[0].start_time, :end_time=>ti.start_time})
-    #  end
-    #    toAdd << capacity_intervals.new({:capacity=>intervals[0].capacity-ti.capacity, :start_time=>intervals[0].start_time, :end_time=>ti.start_time})
-    #  if(intervals[0].end_time != ti.end_time)
-    #    toAdd << capacity_intervals.new({:capacity=>intervals[0].capacity, :start_time=>ti.end_time, :end_time=>intervals[0].end_time})
-    #  end
-    else #intervals in between
+    else  #both in their own interval
     
+      #Left and Right
+      if(intervals.first.start_time != ti.start_time)
+        toAdd << capacity_intervals.new({:capacity=>intervals.first.capacity, :start_time=>intervals.first.start_time, :end_time=>ti.start_time})
+      end
+        toAdd << capacity_intervals.new({:capacity=>intervals.first.capacity-ti.capacity, :start_time=>ti.start_time, :end_time=>intervals.first.end_time})
+        toAdd << capacity_intervals.new({:capacity=>intervals.first.capacity-ti.capacity, :start_time=>intervals.last.start_time, :end_time=>ti.end_time})
+      if(intervals.last.end_time != ti.end_time)
+        toAdd << capacity_intervals.new({:capacity=>intervals.last.capacity, :start_time=>ti.end_time, :end_time=>intervals.last.end_time})
+      end
+      
+      #Middle
+      if (intervals.size > 2)
+        intervals[1..-2].each{|i| toAdd << capacity_intervals.new({:capacity=>i.capacity - ti.capacity, :start_time=>i.start_time, :end_time=>i.end_time})}
+      end
+      
+      toAdd.each{|i| i.save}
+      intervals.each{|i| i.delete}
     end
   end
   
