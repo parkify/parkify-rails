92a085c4c03c6d7b9e2c2616c928a2bdd275c746
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-08-05 06:48:43 -0700

    Adding ability to make spot from account

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,12 @@ class User < ActiveRecord::Base
       return false
     end
   end
-  
+  def method_missing(meth, *args, &block)
+        puts meth, args, block
+      super # You *must* call super if you don't handle the
+                # method, otherwise you'll mess up Ruby's method
+                # lookup.
+    end
+  end
   
 end
