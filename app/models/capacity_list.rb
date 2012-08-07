18c410340263bb6f4e8858980de256f38cd7f438
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-08-06 23:39:53 -0700

    Adding ability to make spot from account

--- app/models/capacity_list.rb
+++ app/models/capacity_list.rb
@@ -188,6 +188,20 @@ class CapacityList < ActiveRecord::Base
     
     a1.add_if_can!(b4)
   end
+  
+  def self.test9)
+    CapacityList.destroy_all
+    CapacityInterval.destroy_all
+    
+    a1 = CapacityList.create()
+    b1 = a1.capacity_intervals.create({:capacity=>15, :start_time=>Time.at(0), :end_time=>Time.at(5)})
+    b2 = a1.capacity_intervals.create({:capacity=>15, :start_time=>Time.at(5), :end_time=>Time.at(10)})
+    b3 = a1.capacity_intervals.create({:capacity=>15, :start_time=>Time.at(10), :end_time=>Time.at(15)})
+    
+    b4 = CapacityInterval.new({:capacity=>1, :start_time=>Time.at(-2), :end_time=>Time.at(17)})
+    
+    a1.add_if_can!(b4)
+  end
  
   
 end
