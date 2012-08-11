98340ebc6f727dacc3b2888f9f1b705c5f19783b
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-08-11 16:46:49 -0700

    Adding Offer logic

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -9,24 +9,46 @@ class Offer < ActiveRecord::Base
   has_one :capacity_list
   
   def updateWithParent!
-    if capacity == nil
-      capacity = resource.capacity
-    end
-    if location == nil
-      location = resource.location
-    end
-    if price_plan == nil
-      price_plan = resource.price_plan
-    end
-    if capacity_list == nil
+    capacity = resource.capacity
+    
+    
+    location = resource.location
+    
+    
+    price_plan = resource.price_plan
+    
+    
       #TODO: move this logic into constructor for capacity_list
-      capacity_list = CapacityList.create()
-      capacity_list.capacity_intervals.create({:start_time => start_time, :end_time => end_time, :capacity => capacity})
-    end
-    if capacity_list.capacity_intervals = []
-      capacity_list.capacity_intervals.create({:start_time => start_time, :end_time => end_time, :capacity => capacity})
-    end
+    capacity_list = CapacityList.create()
+    capacity_list.capacity_intervals.create({:start_time => start_time, :end_time => end_time, :capacity => capacity})
+    
+    
+    capacity_list.capacity_intervals.create({:start_time => start_time, :end_time => end_time, :capacity => capacity})
+    
     
     save!
   end
+  
+  
+  #def updateWithParent!
+  #  if capacity == nil
+  #    capacity = resource.capacity
+  #  end
+  #  if location == nil
+  #    location = resource.location
+  #  end
+  #  if price_plan == nil
+  #    price_plan = resource.price_plan
+  #  end
+  #  if capacity_list == nil
+  #    #TODO: move this logic into constructor for capacity_list
+  #    capacity_list = CapacityList.create()
+  #    capacity_list.capacity_intervals.create({:start_time => start_time, :end_time => end_time, :capacity => capacity})
+  #  end
+  #  if capacity_list.capacity_intervals = []
+  #    capacity_list.capacity_intervals.create({:start_time => start_time, :end_time => end_time, :capacity => capacity})
+  #  end
+  #  
+  #  save!
+  #end
 end
