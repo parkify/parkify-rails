40918d49f089087f57e5a5d19a5f6aa4322e1b64
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-08-12 09:53:43 -0700

    Adding Offer logic

--- app/models/acceptance.rb
+++ app/models/acceptance.rb
@@ -23,7 +23,7 @@ class Acceptance < ActiveRecord::Base
     amountToCharge = amountToCharge.floor
     start_time = Time.at(start_time)
     end_time = Time.at(end_time)
-    #check if this is a valid request
+    #check if this is a valid request.
     interval = CapacityInterval.new({:start_time => start_time, :end_time => end_time, :capacity => 1})
     puts interval
     puts offer
