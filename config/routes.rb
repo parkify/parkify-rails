5c13de79e1920aca7a3e0893b5ab7e6207f73d13
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-08-12 09:06:24 -0700

    Adding Offer logic

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ ParkifyRails::Application.routes.draw do
       namespace :v1 do
         devise_for :users
         resources :resources, :controller => "parking_spots", :only => [:index, :show]
-        resources :acceptences, :controller => "app_acceptances", :only => [:create]
+        resources :acceptances, :controller => "app_acceptances", :only => [:create]
       end
   end
 
