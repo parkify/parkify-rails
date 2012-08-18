e14d8e1053c0fbd01c6f5501f06d7b46fb11533e
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-08-17 17:30:35 -0700

    Being able to update single spots.

--- app/models/price_interval.rb
+++ app/models/price_interval.rb
@@ -1,4 +1,11 @@
 class PriceInterval < ActiveRecord::Base
   attr_accessible :end_time, :price_per_hour, :price_plan_id, :start_time
   belongs_to :price_plan
+  
+  def as_json(options={})
+    results = super()
+    results[:start_time] = "#{self.start_time.to_f}"
+    results[:end_time] = "#{self.end_time.to_f}"
+    result
+  end
 end
