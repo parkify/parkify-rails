ee9d1d704b0f6b83842cc1d0f3041b41ab9f49ee
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-08-29 09:42:36 -0700

    Finishing acceptance generation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "do-not-reply@parkify.me"
-  
+
   def welcome_email(user)
     @user = user
     mail(:to => user.email, :subject => "Welcome to My Awesome Site")
