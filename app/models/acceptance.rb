d41f1d072bed09b7098b79863f9761f27661e1a5
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-08-28 21:58:10 -0700

    Finishing acceptance generation

--- app/models/acceptance.rb
+++ app/models/acceptance.rb
@@ -9,78 +9,198 @@ class Acceptance < ActiveRecord::Base
   has_many :agreements
   has_many :offers, :through => :agreements
   
-  def self.build_and_charge_from_api(params)
+  # Just builds a blank acceptance, since acceptances need to be saved first before
+  #   we can add member entities.
+  def self.build_from_api(params)
     params = JSON.parse(params)
-    logger.info "TEST: #{params}"
-    user = User.find_by_authentication_token(params["authentication_token"])
-    puts "--------------------------------"
-    puts user
-    puts params["authentication_token"]
-    puts "--------------------------------"
-    spot = Resource.find_by_id(params["parking_spot_id"].to_i)
     
-    customer = user.stripe_customer_ids.first #TODO: pick the one that is active instead
-    start_time = params["start_time"].to_f()
-    end_time = params["end_time"].to_f()
+    toRtn = new()
+    toRtn.status = "pending"
+    toRtn.user = User.find_by_authentication_token(params["authentication_token"])
+    toRtn.start_time = params["start_time"].to_f()
+    toRtn.end_time = params["end_time"].to_f()
     
+    if(toRtn.end_time <= toRtn.start_time)
+      return nil
+    end
+    
+    toRtn
+  end
+  
+  # Now try to validate, find the total cost, and charge with card.
+  # If anything goes wrong, clean up and return false.
+  # Assumes: that self has been saved.
+  def add_offers_and_charge_from_api(params)
+    params = JSON.parse(params)
+    
+    if(!self.user)
+      self.status = "invalid user"
+      return false
+    end
+    
+    b_undo = false
     amountToCharge = 0.0
+    offers_to_add = []
     params["offer_ids"].each do |offer_id|
     
       #grab the correct offer
       offer = Offer.find_by_id(offer_id.to_i)
-      self.offers << offer
+      offers_to_add << offer
       
-      # check if there is space
-      #eff_start_time = MAX(start_time, 
-      #interval = CapacityInterval.new({:start_time => eff_start_time, :eff_end_time => end_time, :capacity => 1})
-      #offer.capacity_list.add_if_can!(interval)
+      #check if there is space
+      eff_start_time = MAX(self.start_time, offer.start_time)
+      eff_end_time = MIN(self.end_time, offer.end_time)
+      if(eff_end_time < start_time)
+        b_undo = true
+        self.status = "invalid timing"
+        break 
+      end
+      interval = CapacityInterval.new({:start_time => eff_start_time, :end_time => eff_end_time, :capacity => 1})
+      if(!offer.capacity_list.add_if_can!(interval))
+        b_undo = true
+        self.status = "not enough capacity"
+        break
+      end
       
       # find cost
-      amountToCharge += offer.find_cost(start_time, end_time)
+      amountToCharge += offer.find_cost(eff_start_time, eff_end_time)
     end
-    
-    
-    
-    
-    amountToCharge += findAmountToCharge
-    amountToCharge = offer.price_plan.price_per_hour * (end_time - start_time) / 36 #convert to cents
-    amountToCharge = amountToCharge.floor
-    start_time = Time.at(start_time)
-    end_time = Time.at(end_time)
-    #check if this is a valid request.
-    interval = CapacityInterval.new({:start_time => start_time, :end_time => end_time, :capacity => 1})
-    puts interval
-    puts offer
-    if(offer.capacity_list.add_if_can!(interval))
-      puts "Got here"
-      toRtn = new()
-      toRtn.user_id = user.id
       
-      toRtn.offer_id = offer.id
-      toRtn.start_time = Time.at(start_time)
-      toRtn.end_time = Time.at(end_time)
-      toRtn.status = "successfully_paid"
-      toRtn.count = 1
-      
-      paymentInfo = PaymentInfo.new
-      paymentInfo.stripe_customer_id_id = customer.id
-      paymentInfo.amount_charged = amountToCharge
-      
-      #do charging
-      
-      charge = Stripe::Charge.create ({:amount=>amountToCharge, :currency=>"usd", :customer => customer.customer_id, :description => user.email})
-
-      if(charge.failure_message.nil?)
-        toRtn.status = "successfully_paid"
-      else
-        toRtn.status = "not_successfully_paid"
-        interval.capacity = -interval.capacity
+    #uh oh, we failed somewhere. Time to clean up our mess.
+    if(b_undo)
+      offers_to_add[0...-1].each do |offer|
+        eff_start_time = MAX(self.start_time, offer.start_time)
+        eff_end_time = MIN(self.end_time, offer.end_time)
+        interval = CapacityInterval.new({:start_time => eff_start_time, :end_time => eff_end_time, :capacity => -1})
         offer.capacity_list.add_if_can!(interval)
       end
-      paymentInfo.stripe_charge_id = charge.id
-      return [toRtn, paymentInfo]
+      return false
     else
-      return [nil, nil]
+      offers_to_add.each do |offer|
+        self.offers << offer
+      end
+    end
+  
+    #now focus on payment
+    customer = user.stripe_customer_ids.first #TODO: pick the one that is active instead
+    paymentInfo = self.payment_info_build()
+    paymentInfo.stripe_customer_id_id = customer.id
+    paymentInfo.amount_charged = amountToCharge
+    
+    charge = Stripe::Charge.create ({:amount=>amountToCharge, :currency=>"usd", :customer => customer.customer_id, :description => user.email})
+
+    if(charge.failure_message.nil?)
+      toRtn.status = "successfully_paid"
+    else
+      toRtn.status = "not_successfully_paid"
+      interval.capacity = -interval.capacity
+      offer.capacity_list.add_if_can!(interval)
     end
   end
+
+
+  #def self.build_and_charge_from_api(params)
+  #  b_undo = false
+  #  params = JSON.parse(params)
+  #  logger.info "TEST: #{params}"
+  #  user = User.find_by_authentication_token(params["authentication_token"])
+  #  puts "--------------------------------"
+  #  puts user
+  #  puts params["authentication_token"]
+  #  puts "--------------------------------"
+  #  spot = Resource.find_by_id(params["parking_spot_id"].to_i)
+  #  
+  #  customer = user.stripe_customer_ids.first #TODO: pick the one that is active instead
+  #  start_time = params["start_time"].to_f()
+  #  end_time = params["end_time"].to_f()
+  #  
+  #  amountToCharge = 0.0
+  #  params["offer_ids"].each do |offer_id|
+  #  
+  #    #grab the correct offer
+  #    offer = Offer.find_by_id(offer_id.to_i)
+  #    self.offers << offer
+  #    
+  #    #check if there is space
+  #    eff_start_time = MAX(start_time, offer.start_time)
+  #    eff_end_time = MIN(end_time, offer.end_time)
+  #    if(eff_end_time < start_time)
+  #      b_undo = true
+  #      break 
+  #    end
+  #    interval = CapacityInterval.new({:start_time => eff_start_time, :end_time => eff_end_time, :capacity => 1})
+  #    if(!offer.capacity_list.add_if_can!(interval))
+  #      b_undo = true
+  #      break
+  #    end
+  #    
+  #    # find cost
+  #    amountToCharge += offer.find_cost(eff_start_time, eff_end_time)
+  #  end
+  #  
+  #  #uh oh, we failed somewhere. Time to clean up our mess.
+  #  if(b_undo)
+  #    self.offers[0...-1].each do |offer|
+  #      
+  #    end
+  #    return [nil,nil]
+  #  end
+  #      
+  #  paymentInfo = PaymentInfo.new
+  #  paymentInfo.stripe_customer_id_id = customer.id
+  #  paymentInfo.amount_charged = amountToCharge
+  #  
+  #  charge = Stripe::Charge.create ({:amount=>amountToCharge, :currency=>"usd", :customer => customer.customer_id, :description => user.email})
+  #
+  #  if(charge.failure_message.nil?)
+  #    toRtn.status = "successfully_paid"
+  #  else
+  #    toRtn.status = "not_successfully_paid"
+  #    interval.capacity = -interval.capacity
+  #    offer.capacity_list.add_if_can!(interval)
+  #  end
+  #  
+  #  return [self, paymentInfo]
+  #  
+  #  amountToCharge += findAmountToCharge
+  #  amountToCharge = offer.price_plan.price_per_hour * (end_time - start_time) / 36 #convert to cents
+  #  amountToCharge = amountToCharge.floor
+  #  start_time = Time.at(start_time)
+  #  end_time = Time.at(end_time)
+  #  #check if this is a valid request.
+  #  interval = CapacityInterval.new({:start_time => start_time, :end_time => end_time, :capacity => 1})
+  #  puts interval
+  #  puts offer
+  #  if(offer.capacity_list.add_if_can!(interval))
+  #    puts "Got here"
+  #    toRtn = new()
+  #    toRtn.user_id = user.id
+  #    
+  #    toRtn.offer_id = offer.id
+  #    toRtn.start_time = Time.at(start_time)
+  #    toRtn.end_time = Time.at(end_time)
+  #    toRtn.status = "successfully_paid"
+  #    toRtn.count = 1
+  #    
+  #    paymentInfo = PaymentInfo.new
+  #    paymentInfo.stripe_customer_id_id = customer.id
+  #    paymentInfo.amount_charged = amountToCharge
+  #    
+  #    #do charging
+  #    
+  #    charge = Stripe::Charge.create ({:amount=>amountToCharge, :currency=>"usd", :customer => customer.customer_id, :description => user.email})
+  #
+  #    if(charge.failure_message.nil?)
+  #      toRtn.status = "successfully_paid"
+  #    else
+  #      toRtn.status = "not_successfully_paid"
+  #      interval.capacity = -interval.capacity
+  #      offer.capacity_list.add_if_can!(interval)
+  #    end
+  #    paymentInfo.stripe_charge_id = charge.id
+  #    return [toRtn, paymentInfo]
+  #  else
+  #    return [nil, nil]
+  #  end
+  #end
 end
