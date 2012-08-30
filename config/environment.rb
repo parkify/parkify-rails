76ba7d96e0272d107628ebf1c5e7cd4ea1c2eca0
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-08-29 20:34:50 -0700

    Adding new static website

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 ParkifyRails::Application.initialize!
+
+#Specify the encoding
+Encoding.default_external = "UTF-8"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 ParkifyRails::Application.configure do
+
+
+
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
