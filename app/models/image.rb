0fdcf81937288e156712658fb864cd1456ac3776
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-08-31 23:39:46 -0700

    Adding spot images

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,5 +2,5 @@ class Image < ActiveRecord::Base
   belongs_to :imageable, :polymorphic => true
   has_attached_file :image_attachment
   
-  attr_accessible :description, :name, :path, :image_attachment
+  attr_accessible :description, :name, :path, :image_attachment, :imageable
 end
