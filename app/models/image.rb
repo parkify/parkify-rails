c92a09e46a17b46d2feaed8c7d6db33b9c402f3d
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-09-01 00:51:57 -0700

    Adding spot images

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,11 @@
 class Image < ActiveRecord::Base
   belongs_to :imageable, :polymorphic => true
-  has_attached_file :image_attachment
+  :has_attached_file :image_attachment, :style => {:spot_view_small => "118x96"},
+    :url  => "/assets/image_attachments/:id/:style/:basename.:extension",
+    :path => ":rails_root/public/assets/image_attachments/:id/:style/:basename.:extension"
+
+  validates_attachment_size :image_attatchment, :less_than => 5.megabytes
+  validates_attachment_content_type :image_attachment, :content_type => ['image/jpeg', 'image/png']
   
   attr_accessible :description, :name, :path, :image_attachment, :imageable
 end
