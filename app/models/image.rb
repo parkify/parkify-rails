36e181bae77606ed3e605f47f4d005af67521245
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-09-04 09:18:34 -0700

    Adding spot images

--- app/models/image.rb
+++ app/models/image.rb
@@ -14,4 +14,14 @@ class Image < ActiveRecord::Base
   validates_attachment_content_type :image_attachment, :content_type => ['image/jpeg', 'image/png']
   
   attr_accessible :description, :name, :path, :image_attachment, :imageable
+  
+  attr_accessor :copy_of
+  
+  def image_attachment_url
+    if copy_of
+      copy_of.url
+    else
+      image.url
+    end
+  end
 end
