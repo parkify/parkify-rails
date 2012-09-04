5159e346e73ca3dd1033a7b9664ef0d76ded4c42
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-09-04 08:43:27 -0700

    Adding spot images

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -20,6 +20,7 @@ class Resource < ActiveRecord::Base
     result["location"] = self.location.as_json(options)
     
     imageIDs = []
+    p self.images
     self.images.each do |i|
       imageIDs << i.id
     end
