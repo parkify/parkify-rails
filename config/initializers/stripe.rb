ab4ab2935916b4e9d873f8647cd78d790419e867
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-09-05 00:17:24 -0700

    Adding spot images

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,2 @@
-Stripe.api_key = "yX8LAOzSjtx7wtZOK2HQnKe2CU772oFm"
-STRIPE_PUBLIC_KEY = "pk_GP95lUPyExWOy8e81qL5vIbwMH7G8"
+Stripe.api_key = "S27kWbmJvSItmxe9xl2XWS9OOqGJcjtE"
+STRIPE_PUBLIC_KEY = "pk_XeTF5KrqXMeSyyqApBF4q9qDzniMn"
