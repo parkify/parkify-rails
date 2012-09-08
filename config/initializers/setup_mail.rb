f0631259e0a798b56888300f149b5a843289751a
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-09-07 23:37:16 -0700

    Tightening mailing

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => "railscasts.com",
-  :user_name            => "railscasts",
-  :password             => "secret",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
+  :domain               => 'parkify.me',
+  :user_name            => 'you-are-a-valued-customer@parkify.me',
+  :password             => 'JustKidding',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  
 }
