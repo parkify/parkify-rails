0f2748e92e7a74954df7c3228b4adb07531b43b7
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-09-26 19:57:36 -0700

    Adding spot creation via csv

--- app/controllers/parking_spots_controller.rb
+++ app/controllers/parking_spots_controller.rb
@@ -9,33 +9,42 @@ class ParkingSpotsController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @resource = Resource.new(params[:resource])
-    #@user = User.find_by_email(params[:user][:email])
-
-    @resource.user_id = current_user.id
-    @resource.capacity = 1.0
+    @count = params[:extra][:count]
     
-    respond_to do |format|
-      if @resource.save
-        @location = @resource.build_location(params[:location])
-        @price_plan = @resource.build_price_plan(params[:price_plan])
-            
-        @resource.quick_properties.create({:key => "spot_layout", :value => params[:quick_property][:spot_layout]});
-        @resource.quick_properties.create({:key => "spot_difficulty", :value => params[:quick_property][:spot_difficulty]});
-        @resource.quick_properties.create({:key => "spot_coverage", :value => params[:quick_property][:spot_coverage]});
-        @resource.quick_properties.create({:key => "spot_signage", :value => params[:quick_property][:spot_signage]});
-        @resource.quick_properties.create({:key => "spot_type", :value => "Shared"});
-        
-        if @location.save and @price_plan.save
-          format.html { redirect_to @resource, notice: 'Parking Spot was successfully created.' }
-          format.json { render json: @resource, status: :created, location: @resource }
+    if !@count 
+      format.html { render action: "new" }
+      format.json { render json: {}, status: :unprocessable_entity }
+    end
+    
+    @count.times do 
+      @resource = Resource.new(params[:resource])
+      #@user = User.find_by_email(params[:user][:email])
+
+      @resource.user_id = current_user.id
+      @resource.capacity = 1.0
+      
+      respond_to do |format|
+        if @resource.save
+          @location = @resource.build_location(params[:location])
+          @price_plan = @resource.build_price_plan(params[:price_plan])
+              
+          @resource.quick_properties.create({:key => "spot_layout", :value => params[:quick_property][:spot_layout]});
+          @resource.quick_properties.create({:key => "spot_difficulty", :value => params[:quick_property][:spot_difficulty]});
+          @resource.quick_properties.create({:key => "spot_coverage", :value => params[:quick_property][:spot_coverage]});
+          @resource.quick_properties.create({:key => "spot_signage", :value => params[:quick_property][:spot_signage]});
+          @resource.quick_properties.create({:key => "spot_type", :value => "Shared"});
+          
+          if @location.save and @price_plan.save
+            format.html { redirect_to @resource, notice: 'Parking Spot was successfully created.' }
+            format.json { render json: @resource, status: :created, location: @resource }
+          else
+            format.html { render action: "new" }
+            format.json { render json: @resource.errors, status: :unprocessable_entity }
+          end
         else
           format.html { render action: "new" }
           format.json { render json: @resource.errors, status: :unprocessable_entity }
         end
-      else
-        format.html { render action: "new" }
-        format.json { render json: @resource.errors, status: :unprocessable_entity }
       end
     end
   end
