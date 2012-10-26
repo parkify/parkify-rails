5d54eece7f967ed92f9684f1eed2107093dc5a9d
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-10-26 09:04:52 -0700

    Adding promotions

--- app/models/promo.rb
+++ app/models/promo.rb
@@ -6,20 +6,20 @@ class Promo < ActiveRecord::Base
   has_many :users, :through => :promo_users
   
   def generate_unique_codes(count, personal)
-    if(count == 1)
+    if count == 1
       fallback = 100000
-      while(fallback > 0)
+      while fallback > 0 
         a = SecureRandom::hex(4)
         if(!Code.find_by_code_text(a))
           return [ self.codes.create({:code_text=>a, :personal=>personal}) ]
         end
-        fallback--
+        fallback = fallback - 1
       end
       return nil
     else 
       toRtn = []
       count.times do |c|
-        toRtn += generate_unique_codes(1, personal)
+        toRtn += self.generate_unique_codes(1, personal)
       end
       return toRtn
     end
