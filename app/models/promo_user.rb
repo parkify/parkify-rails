d436cc84cc07ec8a99cb7bbb5d9cdac68ae5d0b3
Dylan Jackson <dylan@hkn.eecs.berkeley.edu>
2012-10-26 08:55:25 -0700

    Adding promotions

--- app/models/promo_user.rb
+++ app/models/promo_user.rb
@@ -1,6 +1,8 @@
 class PromoUser < ActiveRecord::Base
   attr_accessible :code_id, :promo_idd, :user_id
   
-  belongs_to :user, :promo, :code
+  belongs_to :user
+  belongs_to :promo
+  belongs_to :code
   
 end
