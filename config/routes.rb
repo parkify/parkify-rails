c0395d93b2f7714bd7e9b457dea78cad016c57f8
gnamit <gauravnamit@gnamits-MacBook-Pro.local>
2012-11-27 18:54:11 -0800

    added in code for extend. can access by going ot url/extend.html?spotid={spotid} will check if app installed or send to itunes

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ ParkifyRails::Application.routes.draw do
   match "/contact" => "home#contact"
   match "/tos" => "home#tos"
   match "/privacy" => "home#privacy"
+  match "/extend" => "home#extend"
   
   #fix for first iphone release
   match "/users/sign_in" => redirect("http://parkify-rails.herokuapp.com/my/users/sign_in")
@@ -79,6 +80,7 @@ ParkifyRails::Application.routes.draw do
           post 'add_promo', :on => :collection
           post 'update_password', :on => :collection
           post 'reset_password', :on => :collection
+          post 'problem_spot', :on => :collection
         end
       end
   end
