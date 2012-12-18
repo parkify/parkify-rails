b9676200f954982d5c5b2b3a0ff5ff9fd507fc2f
Parkify Rails Box <dylan.r.jackson@gmail.com>
2012-12-17 18:26:41 -0800

    checking for stagingserver environment variable on staging server'

--- config/initializers/00_init.rb
+++ config/initializers/00_init.rb
@@ -2,3 +2,7 @@ pending_migrations = ActiveRecord::Migrator.new(:up, 'db/migrate').pending_migra
 if pending_migrations.any?
   ENV["PENDING_MIGRATIONS"] = "#{true}"
 end
+
+if ENV["STAGING_SERVER"] = "#{true}"
+  ENV["RAILS_ENV"] = "staging"
+end
