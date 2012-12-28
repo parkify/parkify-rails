5f8cf8f8f18efe2f1d2130e78fa19cf30ddbc6a5
Parkify Rails Box <dylan.r.jackson@gmail.com>
2012-12-27 17:18:11 -0800

    Fixing bug: reservation methods error with recent updates to redis

--- app/models/resource_offer_container.rb
+++ app/models/resource_offer_container.rb
@@ -156,7 +156,7 @@ class ResourceOfferContainer
     valid = valid && self.end_time(start_time) >= end_time
     #DEBUG: start
     if(!valid)
-      p ["invalid time in ResourceOfferContainer::validate_reservation", "t_in:", start_time, end_time, "t_my:", self.start_time(start_time), self.end_time(start_time)]
+      p [["invalid time in ResourceOfferContainer::validate_reservation", "t_in:", start_time, end_time, "t_my:", self.start_time(start_time), self.end_time(start_time)], self]
     end
     #DEBUG: end
     return valid
