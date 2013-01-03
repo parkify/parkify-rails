ffdbe8f43a5d2b3a8c9a4610e847b635a07303a0
Parkify Rails Box <dylan.r.jackson@gmail.com>
2013-01-03 13:32:05 -0800

    Fixing issue 1: Not sending email on reset password

--- app/controllers/api/v2/account_controller.rb
+++ app/controllers/api/v2/account_controller.rb
@@ -184,6 +184,7 @@ class Api::V2::AccountController < ApplicationController
     @user = User.find_by_email(params[:email])
     respond_to do |format|
       if @user
+        send_reset_password_instructions
         sign_out @user
         format.json { render json: {:success=>true}, location: @user }
        else
