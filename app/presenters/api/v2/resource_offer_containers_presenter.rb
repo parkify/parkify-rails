a9fd5901195ebb0846b9de11dba19d4abccf6f19
Parkify Rails Box <dylan.r.jackson@gmail.com>
2013-01-06 16:13:36 -0800

    Adding flat-rate prices

--- app/presenters/api/v2/resource_offer_containers_presenter.rb
+++ app/presenters/api/v2/resource_offer_containers_presenter.rb
@@ -100,7 +100,7 @@ class Api::V2::ResourceOfferContainersPresenter < Api::V2::ApplicationPresenter
       "6h" => {:duration => 6.hours, :price => "#{hourly_price*5.5}"},
       "12h" => {:duration => 12.hours, :price => "#{hourly_price*10}"},
       "18h" => {:duration => 18.hours, :price => "#{hourly_price*13.5}"},
-      "24h" => {:duration => 24.hours, :price => "#{hourly_price*16}"},
+      "24h" => {:duration => 24.hours, :price => "#{hourly_price*16}"}
     }
   end
 
