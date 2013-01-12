e6b8f89f5778b7f4ac071baa002f111201faefa3
Parkify Rails Box <dylan.r.jackson@gmail.com>
2013-01-11 17:07:20 -0800

    Adding Flat Rate Pricing

--- db/migrate/20130110021625_create_offer_schedule_flat_rate_prices.rb
+++ db/migrate/20130110021625_create_offer_schedule_flat_rate_prices.rb
@@ -3,7 +3,7 @@ class CreateOfferScheduleFlatRatePrices < ActiveRecord::Migration
     create_table :offer_schedule_flat_rate_prices do |t|
       t.timestamps
       t.references :offer_schedule_flat_rate_priceable, :polymorphic => true
-      t.string :flat_rate_price_id
+      t.integer :flat_rate_price_id
     end
   end
 end
