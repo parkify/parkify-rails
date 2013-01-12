69ce90c824dc2b16621826ecf92f8555a8441b67
Parkify Rails Box <dylan.r.jackson@gmail.com>
2013-01-11 18:02:14 -0800

    Adding flat rate pricing

--- app/models/flat_rate_price.rb
+++ app/models/flat_rate_price.rb
@@ -2,6 +2,11 @@ class FlatRatePrice < ActiveRecord::Base
   attr_accessible :duration, :name, :price
 
   has_many :offer_schedule_flat_rate_prices
-  has_many :offer_schedule_flat_rate_priceables, :through => :offer_schedule_flat_rate_prices
+
+  has_many :offer_schedules, :through => :offer_schedule_flat_rate_prices, :source => :offer_schedule_flat_rate_priceable, :source_type => 'OfferSchedule'
+  has_many :offer_schedule_exceptions, :through => :offer_schedule_flat_rate_prices, :source => :offer_schedule_flat_rate_priceable, :source_type => 'OfferScheduleException'
+
+
+  #has_many :offer_schedule_flat_rate_priceables, :through => :offer_schedule_flat_rate_prices
 
 end
