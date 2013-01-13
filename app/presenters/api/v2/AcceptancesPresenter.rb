9d69dd264f952af7abef9f70a85df6226856d227
Parkify Rails Box <dylan.r.jackson@gmail.com>
2013-01-12 16:09:48 -0800

    Changing status of acceptances on extend

--- app/presenters/api/v2/AcceptancesPresenter.rb
+++ app/presenters/api/v2/AcceptancesPresenter.rb
@@ -2,12 +2,12 @@ class Api::V2::AcceptancesPresenter < Api::V2::ApplicationPresenter
   
   def as_json(acc, options={})
     {
-      :user_id => acc.user_id
-      :resource_offer_id => acc.resource_offer_id
-      :start_time => acc.start_time.to_f
-      :end_time => acc.end_time.to_f
-      :status => acc.status
-      :details => acc.details
+      :user_id => acc.user_id,
+      :resource_offer_id => acc.resource_offer_id,
+      :start_time => acc.start_time.to_f,
+      :end_time => acc.end_time.to_f,
+      :status => acc.status,
+      :details => acc.details,
       :id => acc.id
     }
   end
