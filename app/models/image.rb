1563d51690157b19e0898d86be424008701cde1d
Parkify Rails Box <dylan.r.jackson@gmail.com>
2013-01-18 05:13:36 -0800

    Adding testing for spot images

--- app/models/image.rb
+++ app/models/image.rb
@@ -53,17 +53,16 @@ class Image < ActiveRecord::Base
 
 
   def self.spot_images_are_viewable_test
-    problem_spots = []
-    problem_images = []
+    problem_images = Hash.new([])
 
-    ResourceOffer.each do |ro|
-      ro.images.each do |img|
+    Image.all.each do |img|
+      ro = img.resource_offer
+      if(ro)
         #TODO: make general.
         url = "parkify-rails-staging.herokuapp.com/images/#{img.id}?image_attachment=true&style=original"
         size = FastImage.size(url)
         if !size or size[0] == 0 or size[1] == 0
-          problem_spots << ro
-          problem_images << img
+          problem_images[ro.id] << img.id
         end
       end
     end
@@ -72,9 +71,8 @@ class Image < ActiveRecord::Base
     if problem_spots.size == 0
       print "All spot images are viewable"
     else
-      print "Problem with spot images:\n"
-      p ["Problem Spots", problem_spots.map{|x| x.id}]
-      p ["Problem Images", problem_images.map{|x| x.id}]
+      p "Problem with spot images: "
+      p problem_images
     end
   end
 
