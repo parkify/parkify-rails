e7a91c8214ff5fbf6c16ab70041031927a2668f1
Parkify Rails Box <dylan.r.jackson@gmail.com>
2013-01-22 02:39:40 -0800

    Debugging server (companion to parkify-ios v1.6) backwards compatability issues.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 ParkifyRails::Application.config.secret_token = 752773d4982b014f667e1854af41815cae56850cb0a019b374486954569235fd25527b39c0002cc6bf5d576f0c953b9b63b425bf3e49551b1077dda2b2fdd46d
+
