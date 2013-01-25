f641d1bcc31af2a8975bff82fdeda726733ab5a7
Parkify Rails Box <dylan.r.jackson@gmail.com>
2013-01-25 15:13:12 -0800

    Debuging loading acceptance issue on v2

--- app/presenters/api/v2/acceptances_presenter.rb
+++ app/presenters/api/v2/acceptances_presenter.rb
@@ -4,7 +4,8 @@ class Api::V2::AcceptancesPresenter < Api::V2::ApplicationPresenter
     p ["TRY THIS_____________", acc.resource_offer]
     {
       :user_id => acc.user_id,
-      :resource_offer_id => ((acc.resource_offer.nil?) ? acc.resource_offer_id : acc.resource_offer.sign_id - 10000),
+      #:resource_offer_id => ((acc.resource_offer.nil?) ? acc.resource_offer_id : acc.resource_offer.sign_id - 10000),
+      :resource_offer_id => (acc.resource_offer.sign_id - 10000),
       :start_time => acc.start_time.to_f,
       :end_time => acc.end_time.to_f,
       :status => acc.status,
