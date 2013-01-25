821f218b20b29e28f3fc6709664add066e3ddd94
Parkify Rails Box <dylan.r.jackson@gmail.com>
2013-01-25 03:57:32 -0800

    Fixing backwards compatability bug

--- app/presenters/api/v2/acceptances_presenter.rb
+++ app/presenters/api/v2/acceptances_presenter.rb
@@ -3,7 +3,7 @@ class Api::V2::AcceptancesPresenter < Api::V2::ApplicationPresenter
   def as_json(acc, options={})
     {
       :user_id => acc.user_id,
-      :resource_offer_id => acc.resource_offer_id,
+      :resource_offer_id => ((acc.resource_offer.nil?) ? acc.resource_offer_id : acc.resource_offer.sign_id - 10000),
       :start_time => acc.start_time.to_f,
       :end_time => acc.end_time.to_f,
       :status => acc.status,
