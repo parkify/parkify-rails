210a834eaafa08bddcde7e62b4b26417dd440eb4
Parkify Rails Box <dylan.r.jackson@gmail.com>
2013-02-03 04:48:47 -0800

    Considering whether a spot is active or not for determining validity of reservations

--- app/models/resource_offer_handler.rb
+++ app/models/resource_offer_handler.rb
@@ -71,7 +71,8 @@ class ResourceOfferHandler < Ohm::Model
       return -1
     end
     if(!fresh_container.updated_from_sql && !use_cache)
-      fresh_container.update_from_sql(acceptance.start_time, acceptance.end_time)
+      #fresh_container.update_from_sql(acceptance.start_time, acceptance.end_time)
+      fresh_container.update_from_sql()
     end
     return fresh_container.validate_reservation_and_find_price(acceptance)
   end 
@@ -82,7 +83,8 @@ class ResourceOfferHandler < Ohm::Model
       return -1
     end
     if(!fresh_container.updated_from_sql && !use_cache)
-      fresh_container.update_from_sql(acceptance.start_time, acceptance.end_time)
+      #fresh_container.update_from_sql(acceptance.start_time, acceptance.end_time)
+      fresh_container.update_from_sql()
     end
     return fresh_container.validate_reservation(acceptance)
   end
