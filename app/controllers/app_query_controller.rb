3d02e3028d7fbbef1665edc12db16940d8c2747a
Parkify Rails Box <dylan.r.jackson@gmail.com>
2013-02-04 21:45:28 -0800

    Adding view for query for new users

--- app/controllers/app_query_controller.rb
+++ app/controllers/app_query_controller.rb
@@ -7,12 +7,22 @@ class AppQueryController < ApplicationController
   end
 
   def user_aquisition_create
-    @request = params[:request]
-    @request[:start_time] -= ActiveSupport::TimeZone.new(@request[:time_zone]).utc_offset
-    @request[:end_time] -= ActiveSupport::TimeZone.new(@request[:time_zone]).utc_offset
+    request = params[:request]
+    start_time_parts = []
+    end_time_parts = []
+    for i in 1..5 do
+      start_time_parts << request["start_time(#{i}i)"].to_i
+      end_time_parts << request["end_time(#{i}i)"].to_i
+    end
+    
+    start_time_parts << request["time_zone"]
+    end_time_parts << request["time_zone"]
+
+    start_time = Time.new(*start_time_parts)
+    end_time = Time.new(*end_time_parts)
 
     if(@request)
-      UserMailer.user_aquisition_query_email(@request[:start_time], @request[:start_time], @request[:email]).deliver
+      UserMailer.user_aquisition_query_email(start_time, end_time, request[:email]).deliver
     end
   end
 end
