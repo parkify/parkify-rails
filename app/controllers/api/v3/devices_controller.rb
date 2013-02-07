da65541d41c7133652423c06eb21b6607cfc163e
Parkify Rails Box <dylan.r.jackson@gmail.com>
2013-02-07 13:07:23 -0800

    Fixing testing for trial accounts on new devices

--- app/controllers/api/v3/devices_controller.rb
+++ app/controllers/api/v3/devices_controller.rb
@@ -42,7 +42,7 @@ class Api::V3::DevicesController < ApplicationController
   # Update/Add device record. Check whether or not a trial account is needed to be generated.
   def create(options={})
     new_device = false
-    @device = Device.find_or_initialize_by_device_uid(params[:device_uid], :last_used_at => Time.now)
+    @device = Device.find_or_initialize_by_device_uid(params[:device_uid], :last_used_at => Time.now, :push_token_id => "")
     if(params[:push_token_id])
       @device.push_token_id = params[:push_token_id]
     end
@@ -56,7 +56,7 @@ class Api::V3::DevicesController < ApplicationController
         device_user.last_used_at = Time.now()
         device_user.save
       else
-        @device = Device.find(@device)
+        #@device = Device.find(@device)
         if(@device.users.count == 0)
           new_device = true
         end
